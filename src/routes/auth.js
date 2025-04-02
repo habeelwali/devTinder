cd0dd79004d64eda54732ed8713bc4aa9047b495
LAPTOP-ANCORTTM\habee <habeel497@gmail.com>
2025-04-02 16:09:28 +0400

    update cookie setting

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -72,11 +72,9 @@ authRouter.post("/login", async (req, res) => {
       
       // Updated cookie settings
       res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        httpOnly: false,
+        secure: false,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        domain: process.env.NODE_ENV === "production" ? "http://localhost:5173" : undefined
       });
 
       const userObj = user.toObject();
